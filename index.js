d3a21b225420d83c2e74c85c27919814f697c7e5
Michael Scharl <michael.scharl@me.com>
2016-04-18 17:46:52 +0200

    Init from template

---
+++ index.js
@@ -0,0 +1,10 @@
+const angular      = require('angular'),
+
+      ngModuleName = 'template',
+      ngModule     = angular.module(ngModuleName, []);
+
+// ngModule.service(…)
+// ngModule.directive(…)
+// etc.
+
+module.export = ngModuleName;
